271b3409baf259865ee13d495945d104d1f04059
Ignacio Joaquín Pantoja <pantojaignacio642@gmail.com>
2024-06-26 18:56:12 -0300

    updated header style

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,8 +1,16 @@
+import sol from '/assets/imgs/sol.png'
+import logo from '/assets/imgs/logo.png'
+import '../../styles/main.scss'
+
 function Header() {
   return (
     <>
       <header>
-        <h2>Feria del libro</h2>
+        <nav>
+          <img src={sol} alt="" width={250} />
+        </nav>
+        <h2>Feria del Libro</h2>
+        <img src={logo} alt="" width={250} />
       </header>
     </>
   )
